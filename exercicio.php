b3b89ed84a0fe078241eff3745e9714dd7e02696
devleandrodias <leandrodbdias@gmail.com>
2020-05-11 14:35:48 -0700

    Criando interface gráfica para começar a trabalhar com os exercícios de PHP

---
+++ exercicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <title>Curso PHP</title>
+  </head>
+  <body>
+    <header class="header">
+      <h1>Curso PHP</h1>
+      <h2>Índicie dos Exercícios</h2>
+    </header>
+    <main class="main">
+      <div class="conteudo">
+          Página de Exercícios
+      </div>
+    </main>
+    <footer class="footer">Desenvolvido com ❤ por Leandro Dias em <?= date('Y'); ?></footer>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <title>Curso PHP</title>
+  </head>
+  <body>
+    <header class="header">
+      <h1>Curso PHP</h1>
+      <h2>Índicie dos Exercícios</h2>
+    </header>
+    <main class="main">
+      <div class="conteudo">
+        <nav class="modulos">
+          <div class="modulo">
+            <h3>Módulo 01 - Exercícios</h3>
+            <ul>
+              <li><a href="exercicio.php">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+          <div class="modulo">
+            <h3>Módulo 02 - Exercícios</h3>
+            <ul>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+          <div class="modulo">
+            <h3>Módulo 03 - Exercícios</h3>
+            <ul>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+          <div class="modulo">
+            <h3>Módulo 04 - Exercícios</h3>
+            <ul>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+          <div class="modulo">
+            <h3>Módulo 05 - Exercícios</h3>
+            <ul>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+          <div class="modulo">
+            <h3>Módulo 06 - Exercícios</h3>
+            <ul>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+          <div class="modulo">
+            <h3>Módulo 07 - Exercícios</h3>
+            <ul>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+          <div class="modulo">
+            <h3>Módulo 08 - Exercícios</h3>
+            <ul>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+          <div class="modulo">
+            <h3>Módulo 09 - Exercícios</h3>
+            <ul>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+              <li><a href="#">Exercício A</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </main>
+    <footer class="footer">Desenvolvido com ❤ por Leandro Dias em <?= date('Y'); ?></footer>
+  </body>
+</html>
